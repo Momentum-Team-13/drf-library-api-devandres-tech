3ca74f4b30b72e7d8e88d3e900f9aeb5c240bef3
andres alcocer <devandres.tech@gmail.com>
2022-07-19 16:55:35 -0400

    created booklist view

--- library_api/views.py
+++ library_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from library_api.serializers import BookSerializer
+from library_api.models import Book
+
 
 # Create your views here.
+class BookList(generics.ListCreateAPIView):
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+	def perform_create(self, serializer):
+		serializer.save(author=self.request.user)
