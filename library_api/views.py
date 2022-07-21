35e8362f5c3a103a9b09eaf7632c5552383ed8cf
andres alcocer <devandres.tech@gmail.com>
2022-07-21 14:56:22 -0400

    set owner filter backends

--- library_api/views.py
+++ library_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from library_api.serializers import BookSerializer, BookTrackerSerializer, UserSerializer
 from library_api.models import Book, BookTracker
+from library_api.filters import IsOwnerFilterBackend
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from django.contrib.auth.models import User
@@ -42,16 +43,26 @@ class BookDestroy(generics.DestroyAPIView):
 	serializer_class = BookSerializer
 	permission_classes = [permissions.IsAdminUser]
 
-
+# gets all users in db
 class UserList(generics.ListAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
 
+# lets any auth user create a book tracker and only users can see
+# their own book trackers
+# GET, POST api/book-trackers/
 class BookTrackerListCreate(generics.ListCreateAPIView):
 	queryset = BookTracker.objects.all()
 	serializer_class = BookTrackerSerializer
-	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+	permission_classes = [permissions.IsAuthenticated]
+	filter_backends = [IsOwnerFilterBackend]
 
 	def perform_create(self, serializer):
 		serializer.save(user=self.request.user)
+
+
+class BookTrackerUpdate(generics.UpdateAPIView):
+	queryset = BookTracker.objects.all()
+	serializer_class = BookTrackerSerializer
+	permission_classes = [permissions.IsAuthenticated]
